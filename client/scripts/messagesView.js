150469be256b5b7a289f994e427a3fe3cacc8407
Anna <ann.kazannik@gmail.com>
2020-08-28 17:41:05 -0700

    Implement create method in Parse

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -1,11 +1,23 @@
 var MessagesView = {
-
+  // DOM
   $chats: $('#chats'),
 
   initialize: function() {
   },
 
+  // this function should call template
+  // render the message
   render: function() {
+  },
+
+  renderMessage: function(message) {
+    //message.appendTo($('#chats'));
+    console.log($('#chats'));
+    var newMessage = message.username + ': ' + message.text;
+    console.log(newMessage);
+    ('#chats').append.render(message);
+    console.log(MessagesView.$chats.children());
   }
+
 
 };
--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -3,7 +3,17 @@ var Parse = {
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
   create: function(message, successCB, errorCB = null) {
-    // todo: save a message to the server
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(data) {
+        console.error('chatterbox: Failed to fetch messages', data);
+      }
+    });
   },
 
   readAll: function(successCB, errorCB = null) {
